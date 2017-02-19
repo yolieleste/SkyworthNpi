ccc3cb76535805888c05ff4856aa3190e5b32428
yolieleste <melontomato@gmail.com>
2017-02-18 23:38:00 -0600

    settings modified

--- NPIBlog/settings.py
+++ NPIBlog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'luz18&hrq=#7k3!nrjt$x#=)q(nli2tzqv$10u9g#!!!38p=j_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
